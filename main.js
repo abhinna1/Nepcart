6c14093e9bc3bcb7651aa7bda1a018cc3ae6a91a
abhinna1 <manangharabhinna@gmail.com>
2021-08-07 09:18:01 +0545

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+function login(){
+    var homepage = "Homepage.php";
+    location.href=homepage;
+}
+function showPw() {
+    var x = document.getElementById("pswent");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function connected() {
+    console.log("Connection Established Successfully!");
+}
+function connectionFailed() {
+    console.log("Failed To Establish Connection");
+}
